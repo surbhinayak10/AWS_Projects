bb2acfef457e5b7a03ad8b8af704d2023ea7dbb2
Surbhi Nayak <56524815+surbhinayak10@users.noreply.github.com>
2022-12-07 12:21:48 -0500

    Add files via upload

---
+++ StepFunction_and_Lambda/process_purchase.py
@@ -0,0 +1,28 @@
+import json
+import boto3
+from datetime import datetime
+
+s3 = boto3.client('s3')
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+
+
+def lambda_handler(event, context):
+    
+	bucket ='step-transactions'
+
+	transactionToUpload = {}
+	transactionToUpload['transactionId'] = '12345'
+	transactionToUpload['type'] = event['TransactionType']
+	transactionToUpload['amount'] = 20
+	transactionToUpload['message'] = 'From Process Purchase'
+	transactionToUpload['Time'] = current_time
+
+	fileName = 'purchase/' + current_time + '.json'
+
+	uploadByteStream = bytes(json.dumps(transactionToUpload).encode('UTF-8'))
+
+	s3.put_object(Bucket=bucket, Key=fileName, Body=uploadByteStream)
+
+	print('Put Complete')
+
